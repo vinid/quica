137684c65eab0d2310deb902b534c8bb5a65f2de
vinid <chiccobia@gmail.com>
2020-11-09 12:09:40 +0100

    removing prints from mace

--- quica/internal/measures.py
+++ quica/internal/measures.py
@@ -89,13 +89,11 @@ class Mace(object):
         self.label2int = {label: value for value, label in zip(
             range(self.num_labels), self.unique_labels)}
         self.label2int.update({FILLER: -1})
-        print(self.label2int)
 
 
         # set int to label dict
         self.int2label = {value : label for label,
                                            value in self.label2int.items()}
-        print(self.int2label)
         # translate input labels to indices
         self.labels = self.inputfile.replace(self.label2int).values.astype(int)
 
